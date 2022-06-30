db513018bd1586dbd792f35ed876f0c9884e53df
Frederick Lopez <mtester010@gmail.com>
2022-06-30 15:30:10 -0500

    Added Models as Client, Hotel, Reservation and room. Methods for Hotel are GetClient, Get Room, GetReservation, GetVacantRoom, TopThreeClients and AutomaticReservation.

---
+++ Models/Client.cs
@@ -0,0 +1,17 @@
+﻿namespace SD_115_W22SD_Labs.Models
+{
+    public class Client
+    {
+        public int ClientId { get; set; } = 0;
+        public string Name { get; set; }
+        public long CreditCard { get; set; }
+        public ICollection<Reservation> Reservations { get; set; } = new HashSet<Reservation>();
+
+        public Client(string name, long creditCard)
+        {
+            Name = name;
+            CreditCard = creditCard;
+            ClientId++;
+        }
+    }
+}
---
+++ Models/Reservation.cs
@@ -0,0 +1,18 @@
+﻿namespace SD_115_W22SD_Labs.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public Client Client { get; set; }
+        public Room Room { get; set; }
+        public bool IsCurrent { get; set; }
+        public DateTime DateTime = new DateTime();
+
+        public Reservation (int id, Client client, Room room)
+        {
+            Id = id;
+            Client = client;
+            Room = room;
+        }
+    }
+}
