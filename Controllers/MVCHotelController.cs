cd8450cfecd5b9efa1c3c68d40f02c750470140b
Frederick Lopez <mtester010@gmail.com>
2022-07-03 14:39:31 -0500

    Added All Rooms view

---
+++ Controllers/MVCHotelController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SD_115_W22SD_Labs.Models;
+
+namespace SD_115_W22SD_Labs.Controllers
+{
+    public class MVCHotelController : Controller
+    {
+      public IActionResult AllRooms()
+        {
+            return View(Hotel.Rooms);
+        }
+    }
+}
